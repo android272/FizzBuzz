186d4d3c911e4f6cce355dbb94674b02158f6816
Apullins <android2772@gmail.com>
2019-05-17 00:54:43 -0400

    Made FizzBuzz.fizz return "fizzbuzz" when passed a multiple of 3 and 5.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public String fizz(Integer inputNumber) {
-        return inputNumber.toString();
+        if(inputNumber % 15 == 0) {
+            return "fizzbuzz";
+        } else if(inputNumber % 3 == 0) {
+            return "fizz";
+        } else if(inputNumber % 5 == 0) {
+            return "buzz";
+        } else {
+            return inputNumber.toString();
+        }
     }
 }
