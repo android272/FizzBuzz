4861c0eb05bb382bbe29bf5f3d3c72c77af2e587
Apullins <android2772@gmail.com>
2019-05-17 00:52:32 -0400

    Fixed name of first test. Made FizzBuzz.fizz return "buzz" when passed a multiple of 5.

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -1,19 +1,31 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzbuzz;
+
+    @Before
+    public void setUp() {
+        fizzbuzz = new FizzBuzz();
+    }
+
     @Test
     public void When_FizzBuzz_Is_Passed_A_Number_It_Returns_That_Number() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertEquals("1", fizzbuzz.fizz(1));
         assertEquals("2", fizzbuzz.fizz(2));
     }
 
     @Test
-    public void When_FizzBuzz_Is_Passed_A_Number_That_Is_A_Multiple_Of_5_It_Returns_Buzz() {
-        FizzBuzz fizzbuzz = new FizzBuzz();
+    public void When_FizzBuzz_Is_Passed_A_Number_That_Is_A_Multiple_Of_3_It_Returns_Buzz() {
         assertEquals("fizz", fizzbuzz.fizz(3));
         assertEquals("fizz", fizzbuzz.fizz(6));
+    }
+
+    @Test
+    public void When_FizzBuzz_Is_Passed_A_Number_That_Is_A_Multiple_Of_5_It_Returns_Buzz() {
+        assertEquals("buzz", fizzbuzz.fizz(5));
+        assertEquals("buzz", fizzbuzz.fizz(10));
     }
 }
